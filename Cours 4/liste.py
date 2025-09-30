493dca59e1d3d22e79b5c024da08731c253bd18b
Glory <glorydaniguue423@gmail.com>
2025-09-30 19:59:51 +0200

    Add random selection functionality to liste.py and module.py

---
+++ Cours 4/liste.py
@@ -0,0 +1,11 @@
+import random
+
+financiers = ["Aice","Bob","Charlie","David","Emily"]
+
+
+random_perso = random.int(1,5)
+if random_perso = 1
+
+
+
+print(f"Qui paye :{random_perso}")
---
+++ Cours 4/module.py
@@ -0,0 +1,9 @@
+import random
+
+rando_int =random.randint(1,2)
+if rando_int == 1:
+    print("1")
+    print("Pile")
+else:
+    print("2")  
+    print("Face")    
